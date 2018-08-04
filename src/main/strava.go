20ce5d192b5931144cbb23dc80b8ec285c630627
ctlclarke <ctlclarke@gmail.com>
2018-08-04 19:00:00 +0100

    display ranking and percentile

--- src/main/strava.go
+++ src/main/strava.go
@@ -8,8 +8,10 @@ import (
 )
 func main() {
 
-  var activity r.Activity = api.GetActivity("1720473515")
+  var activity r.Activity = api.GetActivity("1720473837")
   segment_efforts := activity.Segment_efforts
+  var entryRank int
+  var percentile float64
 
   for _, segment_effort := range segment_efforts {
   	fmt.Printf(segment_effort.Segment.Name + ": ")
@@ -17,10 +19,19 @@ func main() {
   	leaderboard := api.GetSegmentLeaderboard(strconv.Itoa(segmentId))
   	for _, entry := range leaderboard.Entries {
         if entry.Athlete_name == "Charles C." {
-            fmt.Printf(strconv.Itoa(entry.Rank))
+            entryRank = entry.Rank
         }
   	}
-  	fmt.Printf(" out of ")
-  	fmt.Printf(strconv.Itoa(leaderboard.Entry_count) + "\n")
+    percentile = 100.0 * float64(entryRank) / float64(leaderboard.Entry_count)
+    fmt.Printf(strconv.Itoa(entryRank))
+    fmt.Printf(" out of ")
+
+    if percentile < 20 {
+      fmt.Printf(strconv.Itoa(leaderboard.Entry_count) + ":  \t\t\t\t  ")
+      fmt.Printf(strconv.FormatFloat(percentile, 'g', 6, 64) + "\n")
+    } else {
+            fmt.Printf(strconv.Itoa(leaderboard.Entry_count) + "\n")
+    }
+
   }
 }
