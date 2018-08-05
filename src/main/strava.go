f2139987b86c309fbdc1dd424693d9dcf49b0a1b
ctlclarke <ctlclarke@gmail.com>
2018-08-05 09:30:23 +0100

    list segments for activity as method

--- src/main/strava.go
+++ src/main/strava.go
@@ -8,30 +8,51 @@ import (
 )
 func main() {
 
-  var activity r.Activity = api.GetActivity("1720473837")
-  segment_efforts := activity.Segment_efforts
-  var entryRank int
-  var percentile float64
+  x := listSegmentsForActivity("1720473837")
 
-  for _, segment_effort := range segment_efforts {
-  	fmt.Printf(segment_effort.Segment.Name + ": ")
-  	segmentId := segment_effort.Segment.Id
-  	leaderboard := api.GetSegmentLeaderboard(strconv.Itoa(segmentId))
-  	for _, entry := range leaderboard.Entries {
+  fmt.Println(x)
+
+
+}
+
+func listSegmentsForActivity(activityId string) []SegmentRankings{
+    var activity r.Activity = api.GetActivity(activityId)
+    var leaderboardPosition int = -1
+    var percentile float64 = -1
+
+    segment_efforts := activity.Segment_efforts
+
+    segmentRankings := make([]SegmentRankings, len(activity.Segment_efforts))
+
+    for index, segment_effort := range segment_efforts {
+      segmentRankings[index].SegmentId = strconv.Itoa(segment_effort.Id)
+      segmentRankings[index].SegmentName = segment_effort.Segment.Name
+
+      segmentId := segment_effort.Segment.Id
+      leaderboard := api.GetSegmentLeaderboard(strconv.Itoa(segmentId))
+
+      for _, entry := range leaderboard.Entries {
         if entry.Athlete_name == "Charles C." {
-            entryRank = entry.Rank
+            leaderboardPosition = entry.Rank
         }
-  	}
-    percentile = 100.0 * float64(entryRank) / float64(leaderboard.Entry_count)
-    fmt.Printf(strconv.Itoa(entryRank))
-    fmt.Printf(" out of ")
+      }
+      percentile = 100.0 * float64(leaderboardPosition) / float64(leaderboard.Entry_count)
 
-    if percentile < 20 {
-      fmt.Printf(strconv.Itoa(leaderboard.Entry_count) + ":  \t\t\t\t  ")
-      fmt.Printf(strconv.FormatFloat(percentile, 'g', 6, 64) + "\n")
-    } else {
-            fmt.Printf(strconv.Itoa(leaderboard.Entry_count) + "\n")
+      segmentRankings[index].Position = leaderboardPosition
+      segmentRankings[index].TotalEfforts = leaderboard.Entry_count
+      segmentRankings[index].Percentile = percentile
+
     }
 
-  }
+    return segmentRankings
+
+    // To return an array of {segmentId, segmentName, position, efforts?, percentile}
+}
+
+type SegmentRankings struct {
+  SegmentId     string
+  SegmentName   string
+  Position      int
+  TotalEfforts  int
+  Percentile    float64
 }
